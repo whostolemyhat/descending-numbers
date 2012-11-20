b7fca0f19bf537b651c9dcdd72447b54a351faa8
James Tease <james@gravitywell.co.uk>
2012-11-20 10:01:41 +0000

    Added description to method

--- descending.py
+++ descending.py
@@ -2,13 +2,21 @@ from sys import argv
 
 
 def descending(n):
-    if n == 1:
-        return n
-    return (n + descending(n - 1))
+    """Adds together numbers from n to 0"""
+    try:
+        n = int(n)
+        if n == 1:
+            return n
+        return (n + descending(n - 1))
+    except ValueError:
+        print "Couldn't convert to integer"
 
 
 def main(argv):
-    print descending(int(argv[1]))
+    try:
+        print descending(int(argv[1]))
+    except ValueError:
+        print "Couldn't convert to integer"
 
 if __name__ == '__main__':
     main(argv)
